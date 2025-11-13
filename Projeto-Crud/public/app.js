2ab17855b9ac636fac224710adc03a9ae81cd865
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-13 16:51:50 -0300

    Update app.js

--- Projeto-Crud/public/app.js
+++ Projeto-Crud/public/app.js
@@ -1,6 +1,10 @@
 //lógica do nosso front-end
 
 const form = document.getElementById('user-form');
+const userList = document.getElementById('user-list');
+
+//chamar a função para carregar os usuários:
+carregarUsuarios();
 
 //evento: submit
 form.addEventListener('submit', e =>{
@@ -26,5 +30,53 @@ function cadastrarUsuario(nome, email) {
     })
     .then(()=>{
         form.reset();
+        carregarUsuarios();
+    })
+}
+
+//função carregar os usuários
+function carregarUsuarios(){
+    fetch('/api/users/') //por padrão ele faz a requisição do tipo GET ao servidor
+    .then(res => res.json()) //converte a resposta da requisição para o formato JSON
+    .then(data => { //transforma o JSON em um objeto javascript
+        userList.innerHTML = ''; //inicialmente vazia
+        data.forEach(user =>{
+            const li = document.createElement('li'); //criando um elemento da lista (ul)
+            li.innerHTML = `${user.nome} - ${user.email}
+            <button onclick="atualizarUsuario(${user.id})">Editar</button>
+            <button onclick="excluirUsuario(${user.id})">Excluir</button>`;
+            userList.append(li);
+        });
+    });
+}
+
+//função para editar (atualizar)
+function atualizarUsuario(id) {
+    const nome = prompt('Digite o novo nome:');
+    const email = prompt("Digite o novo e-mail:");
+
+    //requisição ao servidor (backend)
+    fetch(`/api/users/${id}`, {
+        method: 'PUT',
+        headers: {'Content-Type':'application/json'}, //conteudo da msg é um json
+        body: JSON.stringify({nome, email})
+    })
+    .then(()=>{
+        carregarUsuarios();
+    })
+}
+
+//função para excluir (delete)
+function excluirUsuario(id) {
+    const confirmacao = confirm("Tem certeza que deseja excluir este usuário?");
+    if(!confirmacao){
+        return;
+    }
+    
+    fetch(`/api/users/${id}`, {
+        method: 'DELETE'
+    })
+    .then(()=>{
+        carregarUsuarios();
     })
 }
